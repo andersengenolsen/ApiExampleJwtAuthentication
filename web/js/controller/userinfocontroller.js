897644c6390dfd15fc9b0d98cc62a6d52d96ad3e
andersengenolsen <anderseols@gmail.com>
2018-10-28 20:26:08 +0100

    Frontend user summary

--- web/js/controller/userinfocontroller.js
+++ web/js/controller/userinfocontroller.js
@@ -1,12 +1,26 @@
 define(["controller/apicontroller"], function(ApiController) {
 
     const DOMString = {
-        userInfo: ".user-info"
+        usernamewelcome: "#username-welcome",
+        username: "#username",
+        firstname: "#firstname",
+        lastname: "#lastname",
+        email: "#email"
     };
 
+    /*
+     * Populating list with user info, callback function
+     * which is passed to the ApiController.
+     *
+    */
     var loadUserCallback = function(data) {
-        document.querySelector(DOMString.userInfo).innerHTML = JSON.stringify(data);
-        console.log(data);
+
+        document.querySelector(DOMString.username).innerHTML = data["username"];
+        document.querySelector(DOMString.firstname).innerHTML = data["firstName"];
+        document.querySelector(DOMString.lastname).innerHTML = data["lastName"];
+        document.querySelector(DOMString.email).innerHTML = data["email"];
+        document.querySelector(DOMString.usernamewelcome).innerHTML = data["username"];
+        
     }
 
     return {
